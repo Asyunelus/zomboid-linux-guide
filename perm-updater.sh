539fca7a2bfca563d5eda97b65916a3eb2644546
Asyunelus <17104027+Asyunelus@users.noreply.github.com>
2022-12-05 16:22:58 +0900

    Update perm-updater.sh

--- perm-updater.sh
+++ perm-updater.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if (( $EUID != 0 )); then
+    echo "This script must be run as root"
+    exit
+fi
 chmod 744 update-zomboid.sh
 chmod 744 install-package.sh
 chmod 744 first-install.sh
